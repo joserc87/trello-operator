6f04188957fd00dd08d57399c179f910f0a2e760
Jose Ramon Cano <joserc87@gmail.com>
2018-10-09 00:25:12 +0200

    Adding some very basic and wrong click handling

--- trello-operator.js
+++ trello-operator.js
@@ -6,24 +6,54 @@ var $,
     ArrayBuffer,
     Uint8Array,
     Blob,
-    saveAs;
+    saveAs,
+    collapsed = false;
 
-function collapse() {
-    $('.list-card-title').css('display', 'none')
-    $('.badges').css('display', 'none')
-    $('.sticker').css('display', 'none')
-    $('.list-card-details').css('margin', '0')
-    $('.list-card-members').css('display', 'none')
+
+function addEventListeners() {
+  $('.card-label').dblclick(function(e) {
+    //alert('double click!');
+    e.stopPropagation();
+    e.preventDefault();
+    console.log(e);
+      if (collapsed) {
+        expandEverything();
+      } else {
+        collapseEverything();
+      }
+      collapsed = !collapsed;
+      e.stopPropagation();
+  });
+}
+
+function expandEverything() {
+    $('.list-card-title').css('display', 'unset');
+    $('.badges').css('display', 'unset');
+    $('.sticker').css('display', 'unset');
+    $('.list-card-details').css('margin', '0');
+    $('.list-card-members').css('display', 'unset');
+}
+
+function collapseEverything() {
+    $('.list-card-title').css('display', 'none');
+    $('.badges').css('display', 'none');
+    $('.sticker').css('display', 'none');
+    $('.list-card-details').css('margin', '0');
+    $('.list-card-members').css('display', 'none');
+}
+
+function applyExtension() {
+  //collapse()
+  addEventListeners();
 }
 
 // on DOM load
 $(function () {
     "use strict";
-    addInterval = setInterval(collapse, 500);
+    var addInterval = setInterval(applyExtension, 500);
     // Look for clicks on the .js-share class, which is
     // the "Share, Print, Export..." link on the board header option list
     $(document).on('mouseup', function () {
         //addInterval = setInterval(addExportLink, 500);
-        alert('It works!')
     });
 });
