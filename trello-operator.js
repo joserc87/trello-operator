f8cf909e8e0a125af23d931848639b93ed650f32
Jose Ramon Cano <joserc87@gmail.com>
2018-10-09 22:04:34 +0200

    Collapse/expand independent stories

--- trello-operator.js
+++ trello-operator.js
@@ -6,8 +6,16 @@ var $,
     ArrayBuffer,
     Uint8Array,
     Blob,
-    saveAs,
-    collapsed = false;
+    saveAs;
+
+
+function addButtons() {
+  var collapseButtonHTML = '<span class="collapse-button icon-sm icon-down list-card-operation dark-hover js-open-quick-card-editor js-card-menu"></span>';
+  var collapseButton = document.createElement('span');
+  collapseButton.attr('class', "collapse-button icon-sm icon-down list-card-operation dark-hover js-open-quick-card-editor js-card-menu");
+  $('.list-card').append(collapseButton);
+  $('.list-card').attr('style', 'background: red');
+}
 
 
 function addEventListeners() {
@@ -15,15 +23,56 @@ function addEventListeners() {
     //alert('double click!');
     e.stopPropagation();
     e.preventDefault();
-    console.log(e);
-      if (collapsed) {
-        expandEverything();
-      } else {
-        collapseEverything();
-      }
-      collapsed = !collapsed;
-      e.stopPropagation();
+
+    // Story title
+    var title = e.target.title;
+
+    // Check if it's collapsed or expanded
+    var el = $(e.target);
+    var collapsed = el.parent().parent().hasClass('collapsed');
+
+    if (collapsed) {
+      getCardsByTitle(title).removeClass('collapsed');
+      expandStory(title);
+      //expandEverything();
+    } else {
+      getCardsByTitle(title).addClass('collapsed');
+      collapseStory(title);
+      //collapseEverything();
+    }
+    e.stopPropagation();
   });
+}
+
+/**
+ * Given a title, find all the cards that contain a label with that title
+ */
+function getCardsByTitle(title) {
+  function filterByTitle(index, element) {
+    return element.title == title;
+  }
+
+  return $('.card-label').filter(filterByTitle).parent().parent();
+}
+
+function expandStory(title) {
+  console.log('Expanding story "' + title + '"');
+  setDisplay(title, 'unset');
+}
+
+function collapseStory(title) {
+  console.log('Collapsing story "' + title + '"');
+  setDisplay(title, 'none');
+}
+
+function setDisplay(title, display) {
+  var cards = getCardsByTitle(title);
+
+  cards.find('.list-card-title').css('display', display);
+  cards.find('.badges').css('display', display);
+  cards.find('.sticker').css('display', display);
+  //cards.find('.list-card-details').css('margin', '0');
+  cards.find('.list-card-members').css('display', display);
 }
 
 function expandEverything() {
@@ -45,12 +94,13 @@ function collapseEverything() {
 function applyExtension() {
   //collapse()
   addEventListeners();
+  //addButtons();
 }
 
 // on DOM load
 $(function () {
     "use strict";
-    var addInterval = setInterval(applyExtension, 500);
+    var addInterval = setTimeout(applyExtension, 500);
     // Look for clicks on the .js-share class, which is
     // the "Share, Print, Export..." link on the board header option list
     $(document).on('mouseup', function () {
